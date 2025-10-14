d490770399173858566132917d9c820314b10cc0
GalaxyIgor <igornbelisario@gmail.com>
2025-10-14 15:42:49 -0300

    feat: add question 3 and fix: separation of dataset

--- src/analysis/src/main.py
+++ src/analysis/src/main.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Carregar o dataset
-ds = pd.read_csv("../../assets/StudentPerformanceFactors.csv", sep=";")
+ds = pd.read_csv("../../assets/StudentPerformanceFactors.csv", sep=",")
 
 # ================================= 5 Perguntas principais =================================
 # 1.Como a renda familiar influencia o desempenho acadêmico e o acesso a recursos educacionais?
@@ -11,6 +11,36 @@ ds = pd.read_csv("../../assets/StudentPerformanceFactors.csv", sep=";")
 # 2.O envolvimento dos pais e o nível de escolaridade parental reduzem desigualdades de desempenho entre alunos?
 
 # 3.Há disparidades de desempenho entre estudantes de escolas públicas e privadas?
+# Variáveis
+public = ds[ds["School_Type"] == "Public"]["Exam_Score"]
+private = ds[ds["School_Type"] == "Private"]["Exam_Score"]
+medias = ds.groupby("School_Type")["Exam_Score"].mean()
+
+# Console
+print("Notas das escolas publicas:\n")
+print(public,"\n")
+print("Notas das escolas privadas:\n")
+print(private,"\n")
+print("\nMédia das notas por tipo de escola:\n")
+print(medias)
+
+# Grafico
+import matplotlib.pyplot as plt
+
+# gráfico de barras
+plt.figure(figsize=(6, 4))
+medias.plot(kind="bar", color=["skyblue", "salmon"])
+
+plt.title("Média das Notas por Tipo de Escola")
+plt.ylabel("Média do Exam_Score")
+plt.xlabel("Tipo de Escola")
+plt.xticks(rotation=0)
+plt.grid(axis="y", linestyle="--", alpha=0.6)
+
+plt.tight_layout()
+plt.show()
+
+
 
 # 4.Estudantes sem acesso à internet têm desempenho inferior, e isso reflete uma exclusão digital?
 
