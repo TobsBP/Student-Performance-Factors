f055a65a7babb15085fc7eea54e8a62b91e809d4
GalaxyIgor <igornbelisario@gmail.com>
2025-10-16 09:30:00 -0300

    fix: other dataset

--- src/analysis/src/main.py
+++ src/analysis/src/main.py
@@ -3,65 +3,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Carregar o dataset
-ds = pd.read_csv("../../assets/StudentPerformanceFactors.csv", sep=",")
+ds = pd.read_csv("../../assets/student-por.csv", sep=",")
 
 # ================================= 5 Perguntas principais =================================
-# 1.Como a renda familiar influencia o desempenho acadêmico e o acesso a recursos educacionais?
-
-# 2.O envolvimento dos pais e o nível de escolaridade parental reduzem desigualdades de desempenho entre alunos?
-
-# 3.Há disparidades de desempenho entre estudantes de escolas públicas e privadas?
-# Variáveis
-public = ds[ds["School_Type"] == "Public"]["Exam_Score"]
-private = ds[ds["School_Type"] == "Private"]["Exam_Score"]
-medias = ds.groupby("School_Type")["Exam_Score"].mean()
-
-# Console
-print("Notas das escolas publicas:\n")
-print(public,"\n")
-print("Notas das escolas privadas:\n")
-print(private,"\n")
-print("\nMédia das notas por tipo de escola:\n")
-print(medias)
-
-# Grafico de barras
-plt.figure(figsize=(6, 4))
-medias.plot(kind="bar", color=["skyblue", "salmon"])
-
-plt.title("Média das Notas por Tipo de Escola")
-plt.ylabel("Média do Exam_Score")
-plt.xlabel("Tipo de Escola")
-plt.xticks(rotation=0)
-plt.grid(axis="y", linestyle="--", alpha=0.6)
-
-plt.tight_layout()
-plt.show()
-
-
-
-# 4.Estudantes sem acesso à internet têm desempenho inferior, e isso reflete uma exclusão digital?
-
-# 5.A distância da escola impacta negativamente o desempenho e a frequência, especialmente em áreas rurais?
+# 1. Como o nível de escolaridade dos pais influencia o desempenho acadêmico dos estudantes?
+# 2. Há relação entre o tempo de estudo semanal e as notas finais (G3)?
+# 3. O apoio familiar (famsup) afeta positivamente o rendimento escolar?
+# 4. O acesso à internet em casa está associado a melhores resultados escolares?
+# 5. A distância entre casa e escola (traveltime) interfere no desempenho do aluno?
 
 # ================================= 5 Perguntas secundarias =================================
-# 10 Perguntas para Análise do Dataset:
-#     6.Qual a relação entre horas de estudo e desempenho no exame?
-#         Existe correlação positiva entre horas estudadas e notas?
-
-#     7.Como a frequência escolar impacta o desempenho acadêmico?
-#         Estudantes com maior frequência têm notas significativamente melhores?
-
-#     8.Qual gênero apresenta melhor desempenho médio nas avaliações?
-#         Há diferença significativa entre homens e mulheres?
-
-#     9.Qual o perfil de sono dos melhores estudantes?
-#         Há um número ideal de horas de sono associado a melhores notas?
-
-#     10.Qual o nível de motivação mais comum entre os estudantes e como isso afeta suas notas?
-#         A motivação influencia diretamente nos resultados?
-
-#     11.A participação em atividades extracurriculares melhora o desempenho acadêmico?
-#         Estudantes que praticam atividades extras têm melhor rendimento?
-
-#     12.Dentre todos os fatores analisados, qual tem a maior correlação com o sucesso no exame?
-#         Qual é o fator mais determinante para o bom desempenho?
+# 6. O tamanho da família (famsize) impacta o tempo dedicado aos estudos?
+# 7. Alunos que participam de atividades extracurriculares (activities) apresentam melhor desempenho escolar?
+# 8. Há uma correlação entre o consumo de álcool (Dalc, Walc) e as notas finais?
+# 9. O status de relacionamento dos pais (pstatus) influencia a motivação e desempenho dos estudantes?
+# 10. Alunos que desejam cursar o ensino superior (higher) obtêm notas mais altas por maior motivação?
