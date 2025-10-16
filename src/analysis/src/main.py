355022e1302d1fe2a12a309464fc54f6ae8f6a72
GalaxyIgor <igornbelisario@gmail.com>
2025-10-16 15:10:06 -0300

    feat: add comentary

--- src/analysis/src/main.py
+++ src/analysis/src/main.py
@@ -7,14 +7,33 @@ ds = pd.read_csv("../../assets/student-por.csv", sep=",")
 
 # ================================= 5 Perguntas principais =================================
 # 1. Como o nível de escolaridade dos pais influencia o desempenho acadêmico dos estudantes?
+# infos que podem ajudar:
+
 # 2. Há relação entre o tempo de estudo semanal e as notas finais (G3)?
+# infos que podem ajudar: final grade (numeric: from 0 to 20, output target)
+
 # 3. O apoio familiar (famsup) afeta positivamente o rendimento escolar?
+# infos que podem ajudar: famsup=5family educational support (binary: yes or no)
+
 # 4. O acesso à internet em casa está associado a melhores resultados escolares?
+# infos que podem ajudar:
+
 # 5. A distância entre casa e escola (traveltime) interfere no desempenho do aluno?
+# infos que podem ajudar:
 
 # ================================= 5 Perguntas secundarias =================================
 # 6. O tamanho da família (famsize) impacta o tempo dedicado aos estudos?
+# infos que podem ajudar: famsize=family size (binary: 'LE3' - less or equal to 3 or 'GT3' - greater than 3)
+
 # 7. Alunos que participam de atividades extracurriculares (activities) apresentam melhor desempenho escolar?
+# infos que podem ajudar: activities=extra-curricular activities (binary: yes or no)
+
 # 8. Há uma correlação entre o consumo de álcool (Dalc, Walc) e as notas finais?
+# infos que podem ajudar: Dalc=workday alcohol consumption (numeric: from 1 - very low to 5 - very high)
+# Walc=weekend alcohol consumption (numeric: from 1 - very low to 5 - very high)
+
 # 9. O status de relacionamento dos pais (pstatus) influencia a motivação e desempenho dos estudantes?
+# infos que podem ajudar: pstatus=parent's cohabitation status (binary: 'T' - living together or 'A' - apart)
+
 # 10. Alunos que desejam cursar o ensino superior (higher) obtêm notas mais altas por maior motivação?
+# infos que podem ajudar:
